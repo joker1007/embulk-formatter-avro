82ae641c60c1fa9cc247b701f9c445be40e0c488
joker1007 <kakyoin.hierophant@gmail.com>
2017-01-17 05:53:33 +0900

    Implement AvroBooleanConverter (forgetted :sweat:)

--- src/main/java/org/embulk/formatter/avro/converter/AvroBooleanConverter.java
+++ src/main/java/org/embulk/formatter/avro/converter/AvroBooleanConverter.java
@@ -1,6 +1,7 @@
 package org.embulk.formatter.avro.converter;
 
 import org.apache.avro.Schema;
+import org.msgpack.value.Value;
 
 public class AvroBooleanConverter extends AbstractAvroValueConverter {
     public AvroBooleanConverter(Schema schema) {
@@ -8,7 +9,22 @@ public class AvroBooleanConverter extends AbstractAvroValueConverter {
     }
 
     @Override
-    public Object booleanColumn(boolean value) {
-        throw new RuntimeException("Unsupported typecasting");
+    public Boolean booleanColumn(boolean value) {
+        return value;
+    }
+
+    @Override
+    public Boolean longColumn(long value) {
+        return value != 0;
+    }
+
+    @Override
+    public Boolean doubleColumn(double value) {
+        return value != 0;
+    }
+
+    @Override
+    public Boolean stringColumn(String value) {
+        return Boolean.valueOf(value);
     }
 }
